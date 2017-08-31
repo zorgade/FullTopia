4aa4bab0ec6af2cf8bbc3e15fa07f1d4a7cb2dc8
jahmarc <marc@datanet-ch.com>
2017-08-31 19:25:19 +0200

    Ajout d'une communauté

--- app/src/main/java/com/example/fulltopia/fulltopia/Entities/Community.java
+++ app/src/main/java/com/example/fulltopia/fulltopia/Entities/Community.java
@@ -1,5 +1,8 @@
 package com.example.fulltopia.fulltopia.Entities;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Taylan on 18.08.2017.
  */
@@ -7,7 +10,48 @@ package com.example.fulltopia.fulltopia.Entities;
 public class Community {
 
     String name;
-    String dateCreationCommunity;
+    Date dateCreationCommunity;
     String description;
+    List<Activity> activitiesList;
 
+    public Community(){}
+
+    public Community(String name, Date dateCreationCommunity, String description){
+        this.name=name;
+        this.dateCreationCommunity=dateCreationCommunity;
+        this.description=description;
+    }
+
+    public Date getDateCreationCommunity() {
+        return dateCreationCommunity;
+    }
+
+    public void setDateCreationCommunity(Date dateCreationCommunity) {
+        this.dateCreationCommunity = dateCreationCommunity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Activity> getActivitiesList() {
+        return activitiesList;
+    }
+
+    public void setActivitiesList(List<Activity> activitiesList) {
+        this.activitiesList = activitiesList;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
